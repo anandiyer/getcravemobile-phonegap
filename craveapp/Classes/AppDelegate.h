e23f89a553157ded405b8ee97b630ccfc824d596
Brian Samson <brian@briansamson.com>
2011-07-26 12:49:00 -0700

    get things rolling

---
+++ craveapp/Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  craveapp
+//
+//  Created by Brian Samson on 7/20/11.
+//  Copyright Brian Samson Software Consulting 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit craveapp.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
---
+++ craveapp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  craveapp
+//
+//  Created by Brian Samson on 7/20/11.
+//  Copyright Brian Samson Software Consulting 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
